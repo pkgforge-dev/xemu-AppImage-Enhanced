e493c9ddc80da8861afee56689d088d0d5563d39
Link4Electronics <link4electronics@gmail.com>
2026-04-08 08:08:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/xemu
 
 # Additional changes can be done in between here
 
