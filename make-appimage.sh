8f99b90de0bda5ed8f7480453cb9d09f9a6650e0
Link4Electronics <link4electronics@gmail.com>
2026-04-08 08:18:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/xemu.svg
 export DESKTOP=/usr/share/applications/xemu.desktop
+export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
